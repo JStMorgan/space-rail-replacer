9c579f6cf30e639deed9fa9659ba6984eb1ab914
Jake Staberg Morgan <47102583+JStMorgan@users.noreply.github.com>
2023-09-29 18:04:56 +0200

    First working version (v0.1) (#1)
    * First working version
    
    * Removed unnecessary file
    
    * Added thumbnail
    
    * Renamed thumbnail

---
+++ README.md
@@ -0,0 +1,1 @@
+A small mod to replace regular Rail sprites with Space Rails from the Space Exploration mod pack, letting you bring an interplanetary touch to your vanilla or other modded playthroughs.
---
+++ data-updates.lua
@@ -0,0 +1,27 @@
+data.raw["rail-planner"]["rail"].icon = "__space-exploration-graphics__/graphics/icons/space-rail.png"
+data.raw["straight-rail"]["straight-rail"].icon = "__space-exploration-graphics__/graphics/icons/space-rail.png"
+
+data.raw["straight-rail"]["straight-rail"].pictures = rail_pictures_space({
+    {"metals", "metals", mipmap = true},
+    {"backplates", "backplates", mipmap = true},
+    {"ties", "ties", variations = 3},
+    {"stone_path", "stone-path", variations = 3},
+    {"stone_path_background", "stone-path-background", variations = 3},
+    {"segment_visualisation_middle", "segment-visualisation-middle"},
+    {"segment_visualisation_ending_front", "segment-visualisation-ending-1"},
+    {"segment_visualisation_ending_back", "segment-visualisation-ending-2"},
+    {"segment_visualisation_continuing_front", "segment-visualisation-continuing-1"},
+    {"segment_visualisation_continuing_back", "segment-visualisation-continuing-2"}
+  })
+data.raw["curved-rail"]["curved-rail"].pictures = rail_pictures_space({
+    {"metals", "metals", mipmap = true},
+    {"backplates", "backplates", mipmap = true},
+    {"ties", "ties", variations = 3},
+    {"stone_path", "stone-path", variations = 3},
+    {"stone_path_background", "stone-path-background", variations = 3},
+    {"segment_visualisation_middle", "segment-visualisation-middle"},
+    {"segment_visualisation_ending_front", "segment-visualisation-ending-1"},
+    {"segment_visualisation_ending_back", "segment-visualisation-ending-2"},
+    {"segment_visualisation_continuing_front", "segment-visualisation-continuing-1"},
+    {"segment_visualisation_continuing_back", "segment-visualisation-continuing-2"}
+  })
